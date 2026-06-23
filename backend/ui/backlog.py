4ce9a9bf9a43428fc1db2b079f755b2f1aef04e3
lavanyasharmals1312-a11y <lavanyasharma.ls1312@gmail.com>
2026-06-23 15:08:20 +0530

    fixing backlog issue

--- backend/ui/backlog.py
+++ backend/ui/backlog.py
@@ -1,21 +1,26 @@
 import json
+import os
 import streamlit as st
 
-from ui.renderers.backlog_renderer import (
-    render_backlog
-)
+from ui.renderers.backlog_renderer import render_backlog
 
 
 def show_backlog():
 
-    st.title(
-        "Backlog Review"
+    st.title("Backlog Review")
+
+    current_dir = os.path.dirname(__file__)
+
+    json_path = os.path.join(
+        current_dir,
+        "..",
+        "outputs",
+        "project_001.json"
     )
 
-    with open(
-        "outputs/project_001.json"
-    ) as file:
+    json_path = os.path.abspath(json_path)
 
+    with open(json_path, "r") as file:
         data = json.load(file)
 
     render_backlog(data)
